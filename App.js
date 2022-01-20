657623db36307eaacec54911b582f26f8cff0973
Jaideep Singh Garlyal <71918370+Jaideepgarlyal15@users.noreply.github.com>
2022-01-20 23:38:21 +0530

    LinkedIn clone

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "React";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build a linkedin clone</h1>     
+    </div>
+  );
+}
+
+export default App;
